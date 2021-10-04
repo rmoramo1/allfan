c7d2577337c076335eefea83aa2d5b270123644e
unknown <r.mora@succorcenter.com>
2021-10-04 15:12:22 -0600

    tema de bootstrap

--- src/admin.py
+++ src/admin.py
@@ -5,7 +5,7 @@ from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'Cerulean'
+    app.config['FLASK_ADMIN_SWATCH'] = 'slate'
     admin = Admin(app, name='Admin', template_mode='bootstrap3')
 
     
