941c887a24fbdb0b66b6f2bbf05e34a4f03ce09c
unknown <r.mora@succorcenter.com>
2021-12-23 09:47:24 -0600

    props

--- migrations/versions/147707fd66c7_.py
+++ migrations/versions/147707fd66c7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3da578183f05
+Revision ID: 147707fd66c7
 Revises: 
-Create Date: 2021-12-23 09:06:12.696265
+Create Date: 2021-12-23 09:46:41.115591
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3da578183f05'
+revision = '147707fd66c7'
 down_revision = None
 branch_labels = None
 depends_on = None
